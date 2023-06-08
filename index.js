dc31b99993f9986cb132083e1b5f6822405af36c
rfloresj <txlinemusic@gmail.com>
2023-06-07 19:30:06 -0600

    Initial

---
+++ index.js
@@ -0,0 +1,14 @@
+// document.getElementById("count-el").innerText = 5;
+
+let lapsCompleted = 0;
+
+function incrementLap(){
+lapsCompleted = lapsCompleted +1;
+}
+
+incrementLap()
+incrementLap()
+incrementLap()
+
+// console.log(increment());
+console.log(lapsCompleted);
