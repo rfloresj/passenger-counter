0c0caca20f743dd07c87f46953df434dd8321057
rfloresj <rfloresj@Soptecs-MacBook-Pro.local>
2023-06-12 16:57:59 -0500

    Display count in HTML

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // document.getElementById("count-el").innerText = 5;
 
-let lapsCompleted = 0;
+// Initialize the count as 0
 
-function incrementLap(){
-lapsCompleted = lapsCompleted +1;
+// Store the variable to DisplayCount in HTML
+let countEl = document.getElementById("count-el");
+
+console.log(countEl);
+
+let count = 0;
+
+function increment(){
+    count = count + 1;
+    countEl.innerText = count;
+    console.log(count);
 }
-
-incrementLap()
-incrementLap()
-incrementLap()
-
-// console.log(increment());
-console.log(lapsCompleted);
